8777f3ff159b0bd0a40b8bf1ea9b7ef8ca91c60d
thuncol12 <thuncol12@naver.com>
2024-07-09 15:09:24 +0900

    tailwind primary, secondary color 정의

--- lumi-front/tailwind.config.js
+++ lumi-front/tailwind.config.js
@@ -5,7 +5,12 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        primary: "#F0C84B",
+        secondary:"#8583FD"
+      }
+    },
   },
   plugins: [],
 }
