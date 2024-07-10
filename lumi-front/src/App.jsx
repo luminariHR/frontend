3ba3a252aaf12a3a343abc1f40adbcd34906f0a6
thuncol12 <thuncol12@naver.com>
2024-07-10 15:25:26 +0900

    내 근퇴 조회 페이지 라우팅, 설정

--- lumi-front/src/App.jsx
+++ lumi-front/src/App.jsx
@@ -2,15 +2,17 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainPage from "./components/MainPage";
 import DashboardPage from "./components/DashboardPage.jsx";
-import LoginPage from './components/LoginPage';
-import PlatformPage from './components/PlatformPage';
+import LoginPage from "./components/LoginPage";
+import PlatformPage from "./components/PlatformPage";
+import MyAttendancePage from "./components/MyAttendancePage.jsx";
 
 function App() {
   return (
     <Router>
       <Routes>
-          <Route path="/" element={<MainPage />} />
+        <Route path="/" element={<MainPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
+        <Route path="/attendance" element={<MyAttendancePage />} />
         <Route path="/login" element={<LoginPage />} />
         <Route path="/platform" element={<PlatformPage />} />
       </Routes>
