13d5d92d1b24754ff0c79fbc27cf381afe470469
Si Young Byun <70714904+siyoungbyun@users.noreply.github.com>
2024-07-14 00:10:05 +0900

    task: 유저 아톰 기본값 설정

--- lumi-front/src/state/userAtom.js
+++ lumi-front/src/state/userAtom.js
@@ -1,6 +1,13 @@
-import { atom } from "recoil";
+import { atom, selector } from "recoil";
+import { fetchMyProfile } from "../api/userApi.js";
 
 export const loggedInUserState = atom({
   key: "loggedInUserState",
-  default: null,
+  default: selector({
+    key: "loggedInUserState/Default",
+    get: async () => {
+      const me = await fetchMyProfile();
+      return me;
+    },
+  }),
 });
