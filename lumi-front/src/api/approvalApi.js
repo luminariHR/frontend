455df3c9f5d3619e00543b829afff1fbfc084bec
Si Young Byun <70714904+siyoungbyun@users.noreply.github.com>
2024-07-14 15:53:36 +0900

    task: 결재 신청 API 추가

--- lumi-front/src/api/approvalApi.js
+++ lumi-front/src/api/approvalApi.js
@@ -1,37 +1,41 @@
 import axiosInstance from "./axiosInstance";
 
-export const fetchSentRequest = async () => {
+export const requestReview = async (data) => {
   try {
-    const response = await axiosInstance.get(
-      `/approval/documents/sent/`,
-    );
+    const response = await axiosInstance.post(`/approval/documents/`, data);
     return response.data;
   } catch (error) {
-    console.error("에러러러러러ㅓ러러ㅓ러러러ㅓ러러", error);
+    console.error("에러 발생", error);
+    return null;
+  }
+};
+
+export const fetchSentRequest = async () => {
+  try {
+    const response = await axiosInstance.get(`/approval/documents/sent/`);
+    return response.data;
+  } catch (error) {
+    console.error("에러 발생", error);
     return [];
   }
-}
+};
 
 export const fetchRetrievedRequest = async () => {
   try {
-    const response = await axiosInstance.get(
-      `/approval/documents/retrieved/`,
-    );
+    const response = await axiosInstance.get(`/approval/documents/retrieved/`);
     return response.data;
   } catch (error) {
     console.error("에러러러러러ㅓ러러ㅓ러러러ㅓ러러", error);
     return [];
   }
-}
+};
 
 export const fetchReferencedRequest = async () => {
   try {
-    const response = await axiosInstance.get(
-      `/approval/documents/referenced/`,
-    );
+    const response = await axiosInstance.get(`/approval/documents/referenced/`);
     return response;
   } catch (error) {
     console.error("에러러러러러ㅓ러러ㅓ러러러ㅓ러러", error);
     return [];
   }
-}
+};
