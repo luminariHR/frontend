528eb64b0cb52a9cfad3af8b4e0380febb12fac7
thuncol12 <thuncol12@naver.com>
2024-07-15 16:16:50 +0900

    :sparkles: 라우팅 추가

--- lumi-front/src/App.jsx
+++ lumi-front/src/App.jsx
@@ -2,15 +2,26 @@ import React, { Suspense } from "react";
 import { RecoilRoot } from "recoil";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import MainPage from "./components/MainPage";
-const DashboardPage = React.lazy(() => import("./components/DashboardPage.jsx"));
+const DashboardPage = React.lazy(
+  () => import("./components/DashboardPage.jsx"),
+);
 const LoginPage = React.lazy(() => import("./components/LoginPage"));
 const PlatformPage = React.lazy(() => import("./components/PlatformPage"));
 const ChattingPage = React.lazy(() => import("./components/ChattingPage.jsx"));
-const MyAttendancePage = React.lazy(() => import("./components/MyAttendancePage.jsx"));
-const ApprovalPage = React.lazy(() => import("./components/approval/ApprovalPage.jsx"));
+const MyAttendancePage = React.lazy(
+  () => import("./components/MyAttendancePage.jsx"),
+);
+const ApprovalPage = React.lazy(
+  () => import("./components/approval/ApprovalPage.jsx"),
+);
 const CalendarPage = React.lazy(() => import("./components/CalendarPage.jsx"));
-const AdminDepartmentsPage = React.lazy(() => import("./components/AdminDepartmentsPage.jsx"));
+const AdminDepartmentsPage = React.lazy(
+  () => import("./components/AdminDepartmentsPage.jsx"),
+);
 const SignUp = React.lazy(() => import("./components/SignUp.jsx"));
+const AdminRecruitmentPage = React.lazy(
+  () => import("./components/AdminRecruitmentPage.jsx"),
+);
 
 function App() {
   return (
@@ -18,51 +29,86 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<MainPage />} />
-          <Route path="/dashboard" element={
-            <Suspense fallback={<div>Loading...</div>}>
-              <DashboardPage />
-            </Suspense>
-          } />
-          <Route path="/attendance" element={
-            <Suspense fallback={<div>Loading...</div>}>
-              <MyAttendancePage />
-            </Suspense>
-          } />
-          <Route path="/login" element={
-            <Suspense fallback={<div>Loading...</div>}>
-              <LoginPage />
-            </Suspense>
-          } />
-          <Route path="/approval" element={
-            <Suspense fallback={<div>Loading...</div>}>
-              <ApprovalPage />
-            </Suspense>
-          } />
-          <Route path="/platform" element={
-            <Suspense fallback={<div>Loading...</div>}>
-              <PlatformPage />
-            </Suspense>
-          } />
-          <Route path="/chatting" element={
-            <Suspense fallback={<div>Loading...</div>}>
-              <ChattingPage />
-            </Suspense>
-          } />
-          <Route path="/calendar" element={
-            <Suspense fallback={<div>Loading...</div>}>
-              <CalendarPage />
-            </Suspense>
-          } />
-          <Route path="/signup" element={
-            <Suspense fallback={<div>Loading...</div>}>
-              <SignUp />
-            </Suspense>
-          } />
-          <Route path="/admin/departments" element={
-            <Suspense fallback={<div>Loading...</div>}>
-              <AdminDepartmentsPage />
-            </Suspense>
-          } />
+          <Route
+            path="/dashboard"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <DashboardPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/attendance"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <MyAttendancePage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/login"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <LoginPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/approval"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <ApprovalPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/platform"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <PlatformPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/chatting"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <ChattingPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/calendar"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <CalendarPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/signup"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <SignUp />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/admin/departments"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <AdminDepartmentsPage />
+              </Suspense>
+            }
+          />
+          <Route
+            path="/admin/recruitment"
+            element={
+              <Suspense fallback={<div>Loading...</div>}>
+                <AdminRecruitmentPage />
+              </Suspense>
+            }
+          />
         </Routes>
       </Router>
     </RecoilRoot>
