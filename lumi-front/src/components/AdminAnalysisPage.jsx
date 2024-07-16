d72edc23abd70fbf6605c35df0842dff235f9659
thuncol12 <thuncol12@naver.com>
2024-07-16 16:07:55 +0900

    :sparkles: 관리자 자소서 ai 분석 결과 페이지 수정

--- lumi-front/src/components/AdminAnalysisPage.jsx
+++ lumi-front/src/components/AdminAnalysisPage.jsx
@@ -65,95 +65,115 @@ function AdminAnalysisPage() {
           </div>
         </div>
 
-        <div className="overflow-y-auto h-[770px] shadow ">
-          <div className="bg-white p-6">
-            <div className="mb-1 font-semibold">
-              <p className="">{`${applicantData.job_title} 부문`}</p>
-            </div>
-            <div className="flex items-center pb-8 border-b border-gray-200 mb-4 text-sm text-gray-500">
-              <div className="">
-                {`지원자 : ${applicantData.applicant_name} | ${applicantData.applicant_email}`}
+        <div className="relative bg-white overflow-hidden h-[770px] shadow rounded-lg">
+          <div className="absolute top-0 left-0 bottom-0 my-4 mr-4 overflow-y-auto">
+            <div className="bg-white p-6 rounded-lg">
+              <div className="mb-1 font-semibold">
+                <p className="">{`${applicantData.job_title} 부문`}</p>
               </div>
-            </div>
+              <div className="flex items-center pb-8 border-b border-gray-200 mb-4 text-sm text-gray-500">
+                <div className="">
+                  {`지원자 : ${applicantData.applicant_name} | ${applicantData.applicant_email}`}
+                </div>
+              </div>
+              <div className="flex flex-col py-4 mb-4">
+                <div className="mb-6">
+                  <div className="mb-2 text-sm font-semibold">핵심 키워드</div>
+                  <div className="border-gray-400 p-1 rounded-lg ">
+                    <div className="mb-1">
+                      <div className="text-xs mb-1 font-semibold">기술</div>
+                      <div>
+                        {applicantData.keywords.techs
+                          ? applicantData.keywords.techs.map((tech) => (
+                              <Button
+                                key={tech}
+                                text={tech}
+                                size={"sm"}
+                                onClick={() => {}}
+                                addClass="cursor-default mr-2 mb-1"
+                              />
+                            ))
+                          : "기술 키워드가 없습니다"}
+                      </div>
+                    </div>
 
-            <div className="flex flex-col py-4 mb-4 border-b border-gray-200">
-              <div className="mb-6">
-                <div className="mb-2 text-sm font-semibold">핵심 키워드</div>
-                <div className="border-gray-400 p-1 rounded-lg ">
-                  <div className="mb-1">
-                    <div className="text-xs mb-1 font-semibold">기술</div>
                     <div>
-                      {applicantData.keywords.techs
-                        ? applicantData.keywords.techs.map((tech) => (
-                            <Button
-                              key={tech}
-                              text={tech}
-                              size={"sm"}
-                              onClick={() => {}}
-                              addClass="cursor-default mr-2 mb-1"
-                            />
-                          ))
-                        : "기술 키워드가 없습니다"}
+                      <div className="text-xs mb-1 font-semibold">직무</div>
+                      <div>
+                        {applicantData.keywords.jobs
+                          ? applicantData.keywords.jobs.map((tech) => (
+                              <Button
+                                key={tech}
+                                text={tech}
+                                size={"sm"}
+                                onClick={() => {}}
+                                addClass="cursor-default mr-2 mb-1"
+                              />
+                            ))
+                          : "직무 키워드가 없습니다"}
+                      </div>
                     </div>
                   </div>
+                </div>
 
-                  <div>
-                    <div className="text-xs mb-1 font-semibold">직무</div>
-                    <div>
-                      {applicantData.keywords.jobs
-                        ? applicantData.keywords.jobs.map((tech) => (
-                            <Button
-                              key={tech}
-                              text={tech}
-                              size={"sm"}
-                              onClick={() => {}}
-                              addClass="cursor-default mr-2 mb-1"
-                            />
-                          ))
-                        : "직무 키워드가 없습니다"}
-                    </div>
+                <div className="pb-8 mb-8 border-b border-gray-200">
+                  <div className="mb-2 text-sm font-semibold">요약 내용</div>
+                  <div className="text-sm border border-gray-200 rounded-lg p-4 ">
+                    {applicantData.summary}
                   </div>
                 </div>
-              </div>
 
-              <div className="mb-6">
-                <div className="mb-2 text-sm font-semibold">요약 내용</div>
-                <div className="text-sm border border-gray-200 p-4 rounded-lg  ">
-                  {applicantData.summary}
-                </div>
-              </div>
+                {applicantData.questions
+                  ? applicantData.questions.map((item) => (
+                      <div
+                        key={item.question_id}
+                        className=" pb-8 mb-8 border-b border-gray-200"
+                      >
+                        <div className="mb-6 font-semibold">
+                          <span>{item.question_id}. </span>
+                          <span>{item.question}</span>
+                        </div>
+                        <div className="mb-6">
+                          <div className="mb-2 flex items-center">
+                            <div className="text-sm font-semibold mr-2">
+                              맞춤법 검사 결과
+                            </div>
+                            <p className="text-secondary text-xs font-semibold">
+                              교정 {item.wrong_num}건
+                            </p>
+                          </div>
+                          <div
+                            className="text-sm border border-gray-200 p-4 rounded-lg "
+                            dangerouslySetInnerHTML={{
+                              __html: item.spelling,
+                            }}
+                          ></div>
+                        </div>
 
-              <div className="mb-6">
-                <div className="mb-2 flex items-center">
-                  <div className="text-sm font-semibold mr-2">
-                    맞춤법 검사 결과
-                  </div>
-                  <p className="text-secondary text-xs font-semibold">
-                    교정 {applicantData.wrong_num}건
-                  </p>
-                </div>
-                <div
-                  className="text-sm border border-gray-200 p-4 rounded-lg "
-                  dangerouslySetInnerHTML={{ __html: applicantData.spelling }}
-                ></div>
+                        <div>
+                          <div className="mb-2 flex items-center">
+                            <div className="text-sm font-semibold mr-2">
+                              답변 원본
+                            </div>
+                          </div>
+                          <div
+                            className="text-sm border border-gray-200 p-4 rounded-lg "
+                            dangerouslySetInnerHTML={{
+                              __html: item.content,
+                            }}
+                          ></div>
+                        </div>
+                      </div>
+                    ))
+                  : ""}
               </div>
-            </div>
-
-            <div className="mb-6">
-              <div className="mb-2 flex items-center">
-                <div className="text-sm font-semibold mr-2">자소서 원본</div>
+              <div className="flex justify-center pt-5 pb-10">
+                <Button
+                  text={"목록으로"}
+                  variant={"primary"}
+                  onClick={() => navigate(-1)}
+                />{" "}
               </div>
-              <div
-                className="text-sm border border-gray-200 p-4 rounded-lg "
-                dangerouslySetInnerHTML={{ __html: applicantData.content }}
-              ></div>
-            </div>
-            <div className="flex justify-center pt-5 pb-10">
-              <Button
-                text={"목록으로"}
-                variant={"primary"}
-                onClick={() => navigate(-1)}
-              />{" "}
             </div>
           </div>
         </div>
