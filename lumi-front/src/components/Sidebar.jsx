2bc0e1b601d33cf5750e0ba9b851857472ea6a1e
thuncol12 <thuncol12@naver.com>
2024-07-19 14:50:16 +0900

    :poop: 사이드바 부메뉴 추가, 스크롤 안되게 함

--- lumi-front/src/components/Sidebar.jsx
+++ lumi-front/src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
-import React, { createContext, useContext, useState } from "react";
+import React, { createContext, useContext, useEffect, useState } from "react";
 import { AlignJustify, ArrowRightFromLine } from "lucide-react";
 import Avatar from "boring-avatars";
-import { Link, useLocation } from "react-router-dom";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useRecoilValue } from "recoil";
 import { loggedInUserState } from "../state/userAtom.js";
 
@@ -9,6 +9,7 @@ export const SidebarContext = createContext();
 
 export const SidebarProvider = ({ children }) => {
   const [expanded, setExpanded] = useState(false);
+
   return (
     <SidebarContext.Provider value={{ expanded, setExpanded }}>
       {children}
@@ -49,25 +50,29 @@ const Sidebar = ({ children }) => {
             )}
           </button>
         </div>
-        <ul className="flex-1 px-3">{children}</ul>
+        <ul className="flex-1 hide-scrollbar px-3">{children}</ul>
         <div
           className={`flex ${
             expanded ? "p-3" : "justify-center items-center p-3"
           }`}
         >
-          <Link to={'/myprofile'}>
-          {user["profile_image"] ? (
-            <img
-              src={`${user["profile_image"]}`}
-              className={`w-10 h-10 rounded-full ${expanded ? "mr-3" : "mr-0"} cursor-pointer`}
-            />
-          ) : (
-            <Avatar
-              variant="beam"
-              name={`${user.name}`}
-              className={`w-10 h-10 rounded-full ${expanded ? "mr-3" : "mr-0"}`}
-            />
-          )}
+          <Link to={"/myprofile"}>
+            {user["profile_image"] ? (
+              <img
+                src={`${user["profile_image"]}`}
+                className={`w-10 h-10 rounded-full ${
+                  expanded ? "mr-3" : "mr-0"
+                } cursor-pointer`}
+              />
+            ) : (
+              <Avatar
+                variant="beam"
+                name={`${user.name}`}
+                className={`w-10 h-10 rounded-full ${
+                  expanded ? "mr-3" : "mr-0"
+                }`}
+              />
+            )}
           </Link>
           <div
             className={`flex justify-between items-center overflow-hidden transition-all ${
@@ -75,8 +80,8 @@ const Sidebar = ({ children }) => {
             }`}
           >
             <div className="leading-4">
-              <h4 className="font-semibold text-sm  whitespace-nowrap">{`${user.name}`}</h4>
-              <span className="font-light text-[#979797] text-xs  whitespace-nowrap">
+              <h4 className="font-semibold text-sm whitespace-nowrap">{`${user.name}`}</h4>
+              <span className="font-light text-[#979797] text-xs whitespace-nowrap">
                 {`${user["job_title"]}`}
               </span>
             </div>
@@ -89,50 +94,109 @@ const Sidebar = ({ children }) => {
 
 export default Sidebar;
 
-export function SidebarItem({ icon, text, active, alert, to }) {
-  const { expanded } = useContext(SidebarContext);
+export function SidebarItem({
+  icon,
+  text,
+  alert,
+  to,
+  children,
+  hasSubmenu = false,
+}) {
+  const { expanded, setExpanded } = useContext(SidebarContext);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const location = useLocation();
+  const navigate = useNavigate();
+  const isActive =
+    location.pathname === to ||
+    (children &&
+      React.Children.toArray(children).some(
+        (child) => location.pathname === child.props.to,
+      ));
+
+  useEffect(() => {
+    if (children && isActive) {
+      setIsExpanded(true);
+    }
+  }, [children, location.pathname, isActive]);
+
+  const handleItemClick = () => {
+    if (!expanded) {
+      if (hasSubmenu) {
+        setExpanded(true);
+        setIsExpanded(true);
+      } else {
+        navigate(to);
+      }
+    } else if (hasSubmenu) {
+      setIsExpanded(!isExpanded);
+    } else {
+      navigate(to);
+    }
+  };
+
+  useEffect(() => {
+    console.log("expanded", expanded);
+  }, [expanded]);
+
+  return (
+    <li className="relative">
+      <div
+        className={`flex items-center py-2 my-1 font-medium rounded-md cursor-pointer transition-colors group ${
+          isActive
+            ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
+            : "hover:bg-indigo-50 text-gray-600"
+        } ${expanded ? "px-3 " : "justify-center"}`}
+        onClick={handleItemClick}
+      >
+        <div className=" flex justify-center items-center w-full">
+          <div>{icon}</div>
+          <span
+            className={`overflow-hidden transition-all whitespace-nowrap ${
+              expanded ? "w-52 ml-3" : "w-0"
+            }`}
+          >
+            {text}
+          </span>
+          {alert && (
+            <div
+              className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${
+                expanded ? "" : "top-2"
+              }`}
+            ></div>
+          )}
+          {!expanded && (
+            <div
+              className={`absolute left-full rounded-md px-2 py-1 ml-6 bg-indigo-100 text-indigo-800 text-sm
+                    whitespace-nowrap invisible opacity-20 -translate-x-3 transition-all
+                    group-hover:visible group-hover:opacity-100 group-hover:translate-x-0`}
+            >
+              {text}
+            </div>
+          )}
+        </div>
+      </div>
+      {expanded && isExpanded && children && (
+        <ul className="pl-8">{children}</ul>
+      )}
+    </li>
+  );
+}
+
+export function SidebarSubmenu({ text, to }) {
   const location = useLocation();
   const isActive = location.pathname === to;
 
   return (
     <li
-      className={`relative flex items-center py-2 px-3 my-1 font-medium rounded-md cursor-pointer transition-colors group ${
+      className={`h-10 flex items-center ml-6 my-1 pl-4 hover:bg-indigo-50 rounded-lg whitespace-nowrap ${
         isActive
           ? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
           : "hover:bg-indigo-50 text-gray-600"
-      } ${expanded ? "" : "justify-center px-2"}`}
+      }`}
     >
-      <Link to={to}>{icon}</Link>
-      <Link
-        className={`overflow-hidden transition-all whitespace-nowrap ${
-          expanded ? "w-52 ml-3" : "w-0"
-        }`}
-        to={to}
-      >
-        <span
-          className={`overflow-hidden transition-all whitespace-nowrap ${
-            expanded ? "w-52 ml-3" : "w-0"
-          }`}
-        >
-          {text}
-        </span>
+      <Link to={to} className="text-gray-600">
+        {text}
       </Link>
-      {alert && (
-        <div
-          className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${
-            expanded ? "" : "top-2"
-          }`}
-        ></div>
-      )}
-      {!expanded && (
-        <div
-          className={`absolute left-full rounded-md px-2 py-1 ml-6 bg-indigo-100 text-indigo-800 text-sm 
-            whitespace-nowrap invisible opacity-20 -translate-x-3 transition-all 
-            group-hover:visible group-hover:opacity-100 group-hover:translate-x-0`}
-        >
-          {text}
-        </div>
-      )}
     </li>
   );
 }
