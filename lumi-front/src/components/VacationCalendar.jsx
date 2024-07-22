eb40e82fc670fc49dff49b8f25f4f76c961de9b3
thuncol12 <thuncol12@naver.com>
2024-07-22 11:10:11 +0900

    fix: 휴가자 현황 조회 캘린더에 월 변경시 해당 월, 년을 받아오는 로직 추가

--- lumi-front/src/components/VacationCalendar.jsx
+++ lumi-front/src/components/VacationCalendar.jsx
@@ -4,7 +4,11 @@ import dayGridPlugin from "@fullcalendar/daygrid";
 import interactionPlugin from "@fullcalendar/interaction";
 import koLocale from "@fullcalendar/core/locales/ko";
 
-const VacationCalendar = ({ events, handleEventClick }) => {
+const VacationCalendar = ({
+  events,
+  handleEventClick,
+  getThisMonthPtoList,
+}) => {
   return (
     <FullCalendar
       plugins={[dayGridPlugin, interactionPlugin]}
@@ -12,6 +16,16 @@ const VacationCalendar = ({ events, handleEventClick }) => {
       height="100%"
       events={events}
       eventClick={handleEventClick}
+      datesSet={(event) => {
+        let midDate = new Date(
+          (event.start.getTime() + event.end.getTime()) / 2,
+        );
+        let month = midDate.getMonth() + 1;
+        getThisMonthPtoList(month, midDate.getFullYear());
+      }}
+      eventDidMount={(info) => {
+        info.el.style.cursor = "pointer";
+      }}
       locale={koLocale}
       headerToolbar={{
         left: "prev",
