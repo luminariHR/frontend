a8b54e6265c78390eabedc15dd0e5c47c6f1104f
[matsalt] <[minny0919@naver.com]>
2024-07-29 14:01:02 +0900

    fix : 후기 채워넣기

--- lumi-front/src/components/Egg.jsx
+++ lumi-front/src/components/Egg.jsx
@@ -3,6 +3,12 @@ import eggback from "../assets/eggback.png"
 import dv1 from "../assets/dv1.png"
 import dv2 from "../assets/dv2.png"
 import dv3 from "../assets/dv3.png"
+import dv4 from "../assets/dv4.png"
+import dv5 from "../assets/dv5.png"
+import dv6 from "../assets/dv6.png"
+import dv7 from "../assets/dv7.png"
+import dv8 from "../assets/dv8.png"
+import dv9 from "../assets/dv9.png"
 
 const Egg = () => {
   return (
@@ -19,7 +25,13 @@ const Egg = () => {
         <div className='bg-white w-[30%] h-full overflow-auto'>
             <img src={dv1} className="w-full" />
             <img src={dv2} className="w-full object-contain" style={{ height: 'auto' }} />
+            <img src={dv9} className="w-full object-contain" style={{ height: 'auto' }} />
             <img src={dv3} className="w-full object-contain" style={{ height: 'auto' }} />
+            <img src={dv4} className="w-full object-contain" style={{ height: 'auto' }} />
+            <img src={dv5} className="w-full object-contain" style={{ height: 'auto' }} />
+            <img src={dv6} className="w-full object-contain" style={{ height: 'auto' }} />
+            <img src={dv7} className="w-full object-contain" style={{ height: 'auto' }} />
+            <img src={dv8} className="w-full object-contain" style={{ height: 'auto' }} />
         </div>
     </div>
   )
